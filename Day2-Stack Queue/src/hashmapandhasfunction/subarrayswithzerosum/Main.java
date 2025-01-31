7e1e19d854b9c04751b7e3e8b4e30c8b605ed0e3
durgesh656 <durgeshkushwaha656@gmail.com>
2025-01-31 10:53:54 +0530

    Day-2 Done

---
+++ Day2-Stack Queue/src/hashmapandhasfunction/subarrayswithzerosum/Main.java
@@ -0,0 +1,9 @@
+package hashmapandhasfunction.subarrayswithzerosum;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2,-1,4,6,-4,-3,4,-4,-2,2};
+        SubArrays.findZeroSumSubArrays(arr);
+    }
+}
---
+++ Day2-Stack Queue/src/hashmapandhasfunction/subarrayswithzerosum/SubArrays.java
@@ -0,0 +1,30 @@
+package hashmapandhasfunction.subarrayswithzerosum;
+
+
+import java.util.HashMap;
+
+public class SubArrays {
+
+    public static void findZeroSumSubArrays(int[] arr) {
+        HashMap<Integer, Integer> sumMap = new HashMap<>();
+        int sum = 0;
+        // Add the initial cumulative sum to the map
+        sumMap.put(0, -1);
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            // If sum has appeared before, it means there is a zero-sum subarray
+            if (sumMap.containsKey(sum)) {
+                // We found a subarray from the previous occurrence of this cumulative sum to the current index
+                int startIndex = sumMap.get(sum) + 1;
+                // Print the subarray
+                System.out.print("Subarray with zero sum: ");
+                for (int j = startIndex; j <= i; j++) {
+                    System.out.print(arr[j] + " ");
+                }
+                System.out.println();
+            }
+            // Put the current sum with its index in the map
+            sumMap.put(sum, i);
+        }
+    }
+}
