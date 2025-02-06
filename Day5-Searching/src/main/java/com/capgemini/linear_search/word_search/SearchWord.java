2aa6a8450444d3ca434c4a5e0c6ef2694b7140c5
durgesh656 <durgeshkushwaha656@gmail.com>
2025-02-06 10:45:32 +0530

    [Durgesh Kushwaha] Add: Added the solution of search for a word  using linear search problem✨

---
+++ Day5-Searching/src/main/java/com/capgemini/linear_search/word_search/SearchWord.java
@@ -0,0 +1,12 @@
+package com.capgemini.linear_search.word_search;
+
+public class SearchWord {
+    public static String search(String[] arr, String word){
+        int n = arr.length;
+        for (String str : arr) {
+            if (str.contains(word))
+                return "word found : "+str;
+        }
+        return "Not Found";
+    }
+}
